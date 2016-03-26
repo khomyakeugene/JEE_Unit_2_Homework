4a78f14145fcdadb69b5c13e7ff70e77f190415b
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-03-26 14:27:09 +0200

    JEE Unit 2 Homework

---
+++ src/com/company/tasks/implementation/CalcAverageDoubleValueTask.java
@@ -0,0 +1,30 @@
+package com.company.tasks.implementation;
+
+import com.company.tasks.interfaces.Task;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+/**
+ * Created by Yevgen on 26.03.2016 as a part of the project "JEE_Unit_2_Homework".
+ */
+public class CalcAverageDoubleValueTask implements Task<Double> {
+    private double inputData[];
+    private Double result = null;
+
+    public CalcAverageDoubleValueTask(double[] inputData) {
+        this.inputData = inputData;
+    }
+
+    @Override
+    public void execute() {
+        OptionalDouble optionalDoubleResult = Arrays.stream(inputData).average();
+
+        result = optionalDoubleResult.isPresent() ? optionalDoubleResult.getAsDouble() : null;
+    }
+
+    @Override
+    public Double getResult() {
+        return result;
+    }
+}
