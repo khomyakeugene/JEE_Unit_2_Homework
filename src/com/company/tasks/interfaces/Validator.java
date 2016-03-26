c164284e6c52b28e4da0f7f6f351ef1cd7fe4a09
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-03-26 19:50:51 +0200

    JEE Unit 2 Homework

--- src/com/company/tasks/interfaces/Validator.java
+++ src/com/company/tasks/interfaces/Validator.java
@@ -4,6 +4,6 @@ package com.company.tasks.interfaces;
  * Created by Yevgen on 26.03.2016 as a part of the project "JEE_Unit_2_Homework".
  */
 public interface Validator<E> {
-    // Validate delivered value ewwer
+    // Validate delivered value
     boolean isValid(E result);
 }
