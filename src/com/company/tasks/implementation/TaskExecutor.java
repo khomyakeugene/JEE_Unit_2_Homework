f15653603ea90dc82eb8100e91a0d6ea62e35a40
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-03-26 13:43:29 +0200

    Init

---
+++ src/com/company/tasks/implementation/TaskExecutor.java
@@ -0,0 +1,87 @@
+package com.company.tasks.implementation;
+
+import com.company.tasks.interfaces.Executor;
+import com.company.tasks.interfaces.Task;
+import com.company.tasks.interfaces.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Yevgen on 26.03.2016 as a part of the project "JEE_Unit_2_Homework".
+ */
+public class TaskExecutor<E> implements Executor<E> {
+    public static final String METHOD_EXECUTE_HAS_ALREADY_BEEN_CALLED = "Method <execute> has already been called!";
+    public static final String METHOD_EXECUTE_HAS_NOT_BEEN_CALLED = "Method <execute> has not been called!";
+
+    private boolean executeMethodHasBeenCalled = false;
+    private List<Task<? extends E>> tasks = new ArrayList<>();
+    private ArrayList<Validator<E>> validators = new ArrayList<>();
+    private ArrayList<E> validResults = new ArrayList<>();
+    private ArrayList<E> invalidResults = new ArrayList<>();
+
+    private void checkIfExecuteMethodHasBeenCalled() throws Exception  {
+        if (!executeMethodHasBeenCalled) {
+            throw new Exception(METHOD_EXECUTE_HAS_NOT_BEEN_CALLED);
+        }
+    }
+
+    private void checkIfExecuteMethodHasNotBeenCalled()  throws Exception {
+        if (executeMethodHasBeenCalled) {
+            throw new Exception(METHOD_EXECUTE_HAS_ALREADY_BEEN_CALLED);
+        }
+    }
+
+    @Override
+    public void addTask(Task<? extends E> task, Validator<E> validator) throws Exception {
+        checkIfExecuteMethodHasNotBeenCalled();
+
+        tasks.add(task);
+        validators.add(validator);
+    }
+
+    @Override
+    public void addTask(Task<? extends E> task) throws Exception {
+        addTask(task, null);
+    }
+
+    private void executeOneTask(int taskIndex) {
+        Task<? extends E> task = tasks.get(taskIndex);
+        task.execute();
+        E result = task.getResult();
+
+        Validator<E> validator = validators.get(taskIndex);
+        if (validator == null) {
+            validResults.add(result);
+        } else {
+            if (validator.isValid(result)) {
+                validResults.add(result);
+            } else {
+                invalidResults.add(result);
+            }
+        }
+    }
+
+    @Override
+    public void execute() {
+        executeMethodHasBeenCalled = true;
+
+        for (int i = 0; i < tasks.size(); i++) {
+            executeOneTask(i);
+        }
+    }
+
+    @Override
+    public List<E> getValidResults() throws Exception {
+        checkIfExecuteMethodHasBeenCalled();
+
+        return validResults;
+    }
+
+    @Override
+    public List<E> getInvalidResults() throws Exception {
+        checkIfExecuteMethodHasBeenCalled();
+
+        return invalidResults;
+    }
+}
