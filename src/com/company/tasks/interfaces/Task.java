89cb09b8e239d3b5f084fad7f05a937608f2c5fb
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-03-26 19:50:18 +0200

    JEE Unit 2 Homework

--- src/com/company/tasks/interfaces/Task.java
+++ src/com/company/tasks/interfaces/Task.java
@@ -4,7 +4,7 @@ package com.company.tasks.interfaces;
  * Created by Yevgen on 26.03.2016 as a part of the project "JEE_Unit_2_Homework".
  */
 public interface Task<E> {
-    // Start task to execution
+    // Start task to execution erer
     void execute();
 
     // Return execution result
